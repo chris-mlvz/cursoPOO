8f818af123e260c381f02c1405b7c9a6128da2b7
Christian MV <chris.mlvz@gmail.com>
2021-02-15 20:08:50 -0600

    Account - Subclasses - PHP

--- PHP/Account.php
+++ PHP/Account.php
@@ -7,10 +7,13 @@ class Account
     public $email;
     public $password;
 
-    public function __construct($name, $document)
+    public function __construct($id, $name, $document, $email, $password)
     {
+        $this->id = $id;
         $this->name = $name;
         $this->document = $document; 
+        $this->email = $email;
+        $this->password = $password;
     }
 }
 ?>
